1ae9bc55d454dea5cf3975876ec875f1950d811e
Jesse Trimble <jlowelltrim@gmail.com>
2011-06-08 13:33:53 -0600

    Adds caching functionality to Dictionary;
    Adds Dictionary::create to take advantage of caching

--- lib/crypto/dictionary.rb
+++ lib/crypto/dictionary.rb
@@ -6,6 +6,37 @@ module Crypto
 
     WORDS_FILE = File.join(File.dirname(__FILE__), "..", "..", "data", "2of4brif.txt")
 
+    def self.create
+      if cached?
+        load_from_cache
+      else
+        dict = new
+        cache(dict)
+      end
+    end
+
+    def self.cached_dict_path
+      File.expand_path( File.join( File.dirname( __FILE__ ), '../../data/dictionary_cache.yml'))
+    end
+
+    def self.cached?
+      File.exists?(cached_dict_path)
+    end
+
+    def self.cache(dict)
+      unless cached?
+        dict.words_by_pattern("THIS")
+        open(cached_dict_path, "w") do |f|
+          f.puts( YAML::dump(dict) )
+        end
+      end
+      dict
+    end
+
+    def self.load_from_cache
+      YAML::load(File.read(cached_dict_path))
+    end
+
     def initialize(words = nil)
       words = File.open(WORDS_FILE).map(&:strip) if words.nil?
       @words = words.to_set
@@ -29,5 +60,6 @@ module Crypto
       # Don't spew every word
       "Dictionary with #{words.size} words"
     end
+
   end
 end
